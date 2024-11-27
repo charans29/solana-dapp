0abe875377361a61e64ecdc4317fe0936e25c0c1
charans29 <smodugula@csuchico.edu>
2024-11-27 12:49:31 -0500

    add standard versions

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,27 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// import { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-//   webpack(config, { isServer }) {
-//     if (!isServer) {
-//       config.resolve.fallback = {
-//         fs: false,
-//         path: false,
-//         os: false,
-//       };
-//     }
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
-
 import { NextConfig } from 'next';
 import path from 'path';
 
